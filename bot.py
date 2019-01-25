e7f6002318663ec2fd7ab51d2a038ee8bb2857b5
Illya Verbovskyi <43999656+IllyaVerb@users.noreply.github.com>
2019-01-25 17:28:39 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,45 +1,94 @@
-#coding:utf-8
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-import asyncio
-import aiohttp
-from aiohttp import web
-import json
+# This is a simple echo bot using decorators and webhook with CherryPy
+# It echoes any incoming text messages and does not use the polling method.
 
-TOKEN = '663214217:AAErqvYgKbeE1EYLBwh5b4Pds59d1jqltPY'
-API_URL = 'https://api.telegram.org/bot%s/sendMessage' % TOKEN
+import logging
 
-async def handler(request):
-    data = await request.json()
-    headers = {
-        'Content-Type': 'application/json'
-    }
-    message = {
-        'chat_id': data['message']['chat']['id'],
-        'text': data['message']['text']
-    }
-    async with aiohttp.ClientSession(loop=loop) as session:
-        async with session.post(API_URL,
-                                data=json.dumps(message),
-                                headers=headers) as resp:
-            try:
-                assert resp.status == 200
-            except:
-                return web.Response(status=500)
-    return web.Response(status=200)
+import cherrypy
 
-async def init_app(loop):
-    app = web.Application(loop=loop, middlewares=[])
-    app.router.add_post('/api/v1', handler)
-    return app
+import telebot
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    try:
-        app = loop.run_until_complete(init_app(loop))
-        web.run_app(app, host='0.0.0.0', port=23456)
-    except Exception as e:
-        print('Error create server: %r' % e)
-    finally:
-        pass
-    loop.close()
-    
+API_TOKEN = '663214217:AAErqvYgKbeE1EYLBwh5b4Pds59d1jqltPY'
+
+WEBHOOK_HOST = 'https://aiabotpython.herokuapp.com/'
+WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
+
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
+
+# Quick'n'dirty SSL certificate generation:
+#
+# openssl genrsa -out webhook_pkey.pem 2048
+# openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
+#
+# When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
+# with the same value in you put in WEBHOOK_HOST
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
+
+bot = telebot.TeleBot(API_TOKEN)
+
+
+# WebhookServer, process webhook calls
+class WebhookServer(object):
+    @cherrypy.expose
+    def index(self):
+        if 'content-length' in cherrypy.request.headers and \
+           'content-type' in cherrypy.request.headers and \
+           cherrypy.request.headers['content-type'] == 'application/json':
+            length = int(cherrypy.request.headers['content-length'])
+            json_string = cherrypy.request.body.read(length).decode("utf-8")
+            update = telebot.types.Update.de_json(json_string)
+            bot.process_new_updates([update])
+            return ''
+        else:
+            raise cherrypy.HTTPError(403)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message,
+                 ("Hi there, I am EchoBot.\n"
+                  "I am here to echo your kind words back to you."))
+
+
+# Handle all other messages
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+# Remove webhook, it fails sometimes the set if there is a previous webhook
+bot.remove_webhook()
+
+# Set webhook
+bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+                certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
+# Disable CherryPy requests log
+access_log = cherrypy.log.access_log
+for handler in tuple(access_log.handlers):
+    access_log.removeHandler(handler)
+
+# Start cherrypy server
+cherrypy.config.update({
+    'server.socket_host'    : WEBHOOK_LISTEN,
+    'server.socket_port'    : WEBHOOK_PORT,
+    'server.ssl_module'     : 'builtin',
+    'server.ssl_certificate': WEBHOOK_SSL_CERT,
+    'server.ssl_private_key': WEBHOOK_SSL_PRIV
+})
+
+cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
+#13fb73f394c43e1aa17aee0b03f128da7c2a36dafb32aac2afaa39c9cfe6
+
+#API_URL = 'https://api.telegram.org/bot%s/sendMessage' % TOKEN
+
