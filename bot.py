27c2fd15900085ce898e346327a2813ab67ca162
Illya Verbovskyi <43999656+IllyaVerb@users.noreply.github.com>
2019-02-18 20:22:00 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -88,18 +88,18 @@ def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.reply_to(message,
-                 ("Hi there, I am EchoBot.\n"
-"I am here to echo your kind words back to you."))
-
 @server.route("/")
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url="https://aiabotpython.herokuapp.com/") # тут url твого Хіроку додатка
     return "?", 200
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message,
+                 ("Hi there, I am EchoBot.\nI am here to echo your kind words back to you."))
+
 server.run(host="0.0.0.0", port=8443)
 
 
