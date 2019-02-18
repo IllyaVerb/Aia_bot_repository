1be3b579a581ebc67bb24a32e0cbe177873eacf3
Illya Verbovskyi <43999656+IllyaVerb@users.noreply.github.com>
2019-02-18 18:49:23 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,32 +9,24 @@ BOT_URL = 'https://api.telegram.org/bot663214217:AAErqvYgKbeE1EYLBwh5b4Pds59d1jq
 BOT_TOKEN = 'bot663214217:AAErqvYgKbeE1EYLBwh5b4Pds59d1jqltPY'
 
 bot = telebot.TeleBot(BOT_TOKEN)
+    
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
 
-# Перевіряємо чи є змінна середовище Хіроку
-if "HEROKU" in list(process.env.keys()):
-    
-    logger = telebot.logger
-    telebot.logger.setLevel(logging.INFO)
+server = Flask(__name__)
+@server.route("/bot", methods=['POST'])
 
-    server = Flask(__name__)
-    @server.route("/bot", methods=['POST'])
-    
-    def getMessage():
-        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-        return "!", 200
-    
-    @server.route("/")
-    
-    def webhook():
-        bot.remove_webhook()
-        bot.set_webhook(url="https://aiabotpython.herokuapp.com/") # тут url твого Хіроку додатка
-        return "?", 200
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
-    
-else:
-    
-    # Видаляємо про всяк випадок вебхук і запускаємо з звичайним полілнгом.
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+@server.route("/")
+
+def webhook():
     bot.remove_webhook()
-    bot.polling(none_stop=True)
+    bot.set_webhook(url="https://aiabotpython.herokuapp.com/") # тут url твого Хіроку додатка
+    return "?", 200
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+
     
 
