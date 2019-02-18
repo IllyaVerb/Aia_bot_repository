42a703a6d4ae249d593c02fb925bd64a8a06d345
Illya Verbovskyi <43999656+IllyaVerb@users.noreply.github.com>
2019-02-18 20:29:52 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,8 @@ def webhook():
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
-bot.reply_to(message, message.text)
+    bot.reply_to(message, message.text)
+    
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
