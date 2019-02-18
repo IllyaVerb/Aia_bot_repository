800b7815cc174ba731f5d937af104434b878ba9a
Illya Verbovskyi <43999656+IllyaVerb@users.noreply.github.com>
2019-02-18 17:13:34 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@
 
 # This is a simple echo bot using decorators and webhook with CherryPy
 # It echoes any incoming text messages and does not use the polling method.
-import requests  
+import requests
+import telebot
 
 BOT_URL = 'https://api.telegram.org/bot663214217:AAErqvYgKbeE1EYLBwh5b4Pds59d1jqltPY/'
 BOT_TOKEN = 'bot663214217:AAErqvYgKbeE1EYLBwh5b4Pds59d1jqltPY'
