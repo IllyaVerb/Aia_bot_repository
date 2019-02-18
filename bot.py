996238b1bef4e7ff0c3662697928bbd540a09496
Illya Verbovskyi <43999656+IllyaVerb@users.noreply.github.com>
2019-02-18 21:50:39 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,16 +12,10 @@ BOT_TOKEN = 'bot663214217:AAErqvYgKbeE1EYLBwh5b4Pds59d1jqltPY'
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
-
-
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
-
+    bot.reply_to(message, "Hi there, I am EchoBot")
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
