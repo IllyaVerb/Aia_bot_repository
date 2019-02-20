7b7a96098921dd9de7b025a6410931e402a13913
Illya Verbovskyi <43999656+IllyaVerb@users.noreply.github.com>
2019-02-20 17:35:08 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,44 +14,36 @@ BOT_TOKEN = 'bot663214217:AAErqvYgKbeE1EYLBwh5b4Pds59d1jqltPY'
 bot = telebot.TeleBot(BOT_TOKEN)
 
 if "HEROKU" in list(os.environ.keys()):
-    # Handle '/start' and '/help'
-    @bot.message_handler(commands=['help', 'start'])
-    def send_welcome(message):
-        bot.reply_to(message, "Hi there, I am EchoBot")
+    
+    logger = telebot.logger
+    telebot.logger.setLevel(logging.INFO)
 
-    # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-    @bot.message_handler(func=lambda message: True)
+    server = flask.Flask(__name__)
+
+    @server.route("/bot", methods=['POST'])
+    def getMessage():
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
+
+    @server.route("/")
+    def webhook():
+        bot.deleteWebhook()
+        bot.set_webhook(url="https://aiabotpython.herokuapp.com/") # тут url твого Хіроку додатка
+        return "?", 200
+
+    webhook()
+
+    @bot.message_handler(func=lambda message: True, content_types=['text'])
     def echo_message(message):
         bot.reply_to(message, message.text)
 
+    @bot.message_handler(commands=['start'])
+    def handle_start(message):
+        bot.send_message(message.chat.id, 'ПРИВЕТ!')
 
-    bot.polling()
-"""
-logger = telebot.logger
-telebot.logger.setLevel(logging.INFO)
-
-server = flask.Flask(__name__)
-
-@server.route("/bot", methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
-
-@server.route("/")
-def webhook():
-    bot.deleteWebhook()
-    bot.set_webhook(url="https://aiabotpython.herokuapp.com/") # тут url твого Хіроку додатка
-    return "?", 200
-
-webhook()
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
-    
-@bot.message_handler(commands=['start'])
-def handle_start(message):
-    bot.send_message(message.chat.id, 'ПРИВЕТ!')
-
-server.run(host="0.0.0.0", port=8443)
-"""
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', '8443')))
+else:
+    #якщо змінної середовища HEROKU нема, отже запуск з консолі.  
+    # Видаляємо про всяк випадок вебхук і запускаємо з звичайним полілнгом.
+    bot.remove_webhook()
+    bot.polling(none_stop=True)
