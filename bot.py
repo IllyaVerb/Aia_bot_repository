933938bc2e537eb9ec807e5065ebc4a7182da748
Illya Verbovskyi <43999656+IllyaVerb@users.noreply.github.com>
2019-02-22 09:12:28 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,51 @@
+#!/usr/bin/env python
 
 #import logging
 #import os
 #from flask import Flask, request
 #import telebot
 
+import telegram
+from flask import Flask, request
+
 BOT_URL = 'https://api.telegram.org/bot663214217:AAErqvYgKbeE1EYLBwh5b4Pds59d1jqltPY/'
 TOKEN = 'bot663214217:AAErqvYgKbeE1EYLBwh5b4Pds59d1jqltPY'
 
-import os
+app = Flask(__name__)
 
-PORT = int(os.environ.get('PORT', '8443'))
-updater = Updater(TOKEN)
-# add handlers
-updater.start_webhook(listen="0.0.0.0",
-                      port=PORT,
-                      url_path=TOKEN)
-updater.bot.set_webhook("https://aiabotpython.herokuapp.com/" + TOKEN)
-updater.idle()
+global bot
+bot = telegram.Bot(token=TOKEN)
+
+
+@app.route('/HOOK', methods=['POST'])
+def webhook_handler():
+    if request.method == "POST":
+        # retrieve the message in JSON and then transform it to Telegram object
+        update = telegram.Update.de_json(request.get_json(force=True))
+
+        chat_id = update.message.chat.id
+
+        # Telegram understands UTF-8, so encode text for unicode compatibility
+        text = update.message.text.encode('utf-8')
+
+        # repeat the same message back (echo)
+        bot.sendMessage(chat_id=chat_id, text=text)
+
+    return 'ok'
+
+
+@app.route('/set_webhook', methods=['GET', 'POST'])
+def set_webhook():
+    s = bot.setWebhook('https://aiabotpython.herokuapp.com/')
+    if s:
+        return "webhook setup ok"
+    else:
+        return "webhook setup failed"
+
+
+@app.route('/')
+def index():
+return '.'
 
 """
 bot = telebot.TeleBot(BOT_TOKEN)
