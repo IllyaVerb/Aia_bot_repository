a9ba2b190d9fc6273579697bb6823309bd49fee2
Illya Verbovskyi <43999656+IllyaVerb@users.noreply.github.com>
2019-02-22 09:20:32 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ def webhook_handler():
 
 @app.route('/set_webhook', methods=['GET', 'POST'])
 def set_webhook():
-    s = bot.setWebhook('https://aiabotpython.herokuapp.com/')
+    s = bot.setWebhook('https://aiabotpython.herokuapp.com/HOOK')
     if s:
         return "webhook setup ok"
     else:
